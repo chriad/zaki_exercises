59a9ca37f96611cdd6d6870f34b444c9b261523b
chriad <chriad.mueller@stud.unibas.ch>
2014-12-15 16:41:07 +0100

    Added test for function one_hot_encode

--- data_loader.py
+++ data_loader.py
@@ -5,22 +5,26 @@ def load_data(filename):
     return df
 
 def one_hot_encode(df, label):
-    '''Convert column "label" in the df with a one_hot_encoding; 
-    delete column afterwards and append encoded columns; return df'''
-    enc = LabelEncoder()
-    label_encoder = enc.fit(df[label].values)
-    integer_classes = label_encoder.transform(label_encoder.classes_)
-    new_labels = []
-    for i in range(len(integer_classes)):
-        new_labels.append(label+'_'+str(i))
-    enc2 = OneHotEncoder()
-    ohe = enc2.fit(integer_classes.reshape((-1,1)))
-    new_features = ohe.transform(label_encoder.transform(df[label].values).reshape((-1,1)))
-    df = df.drop(label, axis=1)
-    df_one_hot_class = pd.DataFrame(new_features.toarray(), columns=new_labels)
-    df_X = pd.concat([df,df_one_hot_class], axis=1)
-    return df_X
+	    from sklearn.preprocessing import LabelEncoder
+	    from sklearn.preprocessing import OneHotEncoder
+	    '''Convert column "label" in the df with a one_hot_encoding; 
+	    delete column afterwards and append encoded columns; return df'''
+	    enc = LabelEncoder()
+	    label_encoder = enc.fit(df[label].values)
+	    integer_classes = label_encoder.transform(label_encoder.classes_)
+	    new_labels = []
+	    for i in range(len(integer_classes)):
+		new_labels.append(label+'_'+str(i))
+	    enc2 = OneHotEncoder()
+	    ohe = enc2.fit(integer_classes.reshape((-1,1)))
+	    new_features = ohe.transform(label_encoder.transform(df[label].values).reshape((-1,1)))
+	    df = df.drop(label, axis=1)
+	    df_one_hot_class = pd.DataFrame(new_features.toarray(), columns=new_labels)
+	    df_X = pd.concat([df,df_one_hot_class], axis=1)
+	    return df_X
 
 def load_iris_data_frame():
-	pass
+	columns = 'sepal_length,sepal_width,petal_length,petal_width,iris_class'.split(',')
+	df = pd.read_csv('./iris.txt', header=None, names=columns)
+	return df
 
