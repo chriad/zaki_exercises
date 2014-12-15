347bf87e6017e4fe932f8b968a343c802303b240
chriad <chriad.mueller@stud.unibas.ch>
2014-12-15 16:50:47 +0100

    Simplified one_hot_encode with the function fit_transform.

--- data_loader.py
+++ data_loader.py
@@ -10,14 +10,12 @@ def one_hot_encode(df, label):
 	    '''Convert column "label" in the df with a one_hot_encoding; 
 	    delete column afterwards and append encoded columns; return df'''
 	    enc = LabelEncoder()
-	    label_encoder = enc.fit(df[label].values)
-	    integer_classes = label_encoder.transform(label_encoder.classes_)
+	    encoded_labels = enc.fit_transform(df[label].values)
 	    new_labels = []
-	    for i in range(len(integer_classes)):
+	    for i in range(len(enc.classes_)):
 		new_labels.append(label+'_'+str(i))
 	    enc2 = OneHotEncoder()
-	    ohe = enc2.fit(integer_classes.reshape((-1,1)))
-	    new_features = ohe.transform(label_encoder.transform(df[label].values).reshape((-1,1)))
+	    new_features = enc2.fit_transform(encoded_labels.reshape((-1,1)))
 	    df = df.drop(label, axis=1)
 	    df_one_hot_class = pd.DataFrame(new_features.toarray(), columns=new_labels)
 	    df_X = pd.concat([df,df_one_hot_class], axis=1)
